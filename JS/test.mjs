0d95d820332af28aa076c3354ff4c420832358e5
konstantinosporo <konstantinosporo@hotmail.gr>
2023-10-17 23:26:49 +0300

    news

---
+++ JS/test.mjs
@@ -0,0 +1,20 @@
+class User {
+  constructor(userID, userName, userPassword) {
+    this.userID = userID;
+    this.userName = userName;
+    this.userPassword = userPassword;
+  }
+}
+
+function signin(userName, userPassword) {
+  let x = 1;
+}
+
+function signup() {
+  let y = 1;
+}
+
+
+const testuser = new User(1, "kostas", "kk1");
+
+console.log(testuser.userID);
